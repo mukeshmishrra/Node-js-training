de806edb4d9328ecf09c6071aa1a3553d7900e40
Mukeshmishrra <mkmishra9a@gmail.com>
2022-06-25 13:40:21 +0530

    first program on nodejs

---
+++ index.js
@@ -0,0 +1,30 @@
+//----------Basic concepts------------------------- 
+
+console.log("Hello world!!")
+
+//variable and datatype concepts
+let     x = 10;
+let     y = 20;
+const   c = 100
+console.log(c);
+console.log("sum is : " + (x+y));
+
+//try to change const value again
+//c = 120;   // error we cant change the value the const variable.
+//console.log("after changes : " + c);  //error
+
+//let x=50; //error x is already declared
+x = 4;  // output 4
+
+
+
+
+
+
+
+
+
+
+
+
+
