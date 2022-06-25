31f58b1bc0afac7a7685e0da6ff4493e59b7bb5c
Mukeshmishrra <mkmishra9a@gmail.com>
2022-06-25 13:54:47 +0530

    first program on nodejs

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
+/*
+    NODE JS INTRODUCTION
+1.node js is not a programing language.
+2.node js is server environment.
+3.node js can connect with datbase.
+4.code and syntax is very similar to javascript
+5.but not exactly same. (node js we can connect with db (run on server) | javascript- we cant connect with db (Run on browser ))
+6.node js free and open source.
+7.node js uses chrome v8 engine to execute the code.
+8.mostly used for API development
+*/
+
+/*
+    HISTORY OF NODE JS
+1.first release : 27 may 2009
+2.current version: ........
+3.writen in javascript , c++.
+*/
+
+/*
+SETUP | INTALLATION 
+INSTALL NODE JS AND INSTALL VS CODE EDITOR */
+
 //----------Basic concepts------------------------- 
 
 console.log("Hello world!!")
@@ -15,16 +38,3 @@ console.log("sum is : " + (x+y));
 
 //let x=50; //error x is already declared
 x = 4;  // output 4
-
-
-
-
-
-
-
-
-
-
-
-
-
