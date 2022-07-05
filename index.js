78b3ce6d78788b1277f6c39214ef78404625358a
Mukeshmishrra <mkmishra9a@gmail.com>
2022-07-05 14:56:59 +0530

    Express js basic hello world program

--- index.js
+++ index.js
@@ -1,47 +1,19 @@
-// Asynchronous Basic Concepts::
+// Express installation and setup
+// Hello world Example using express js 
 
-/**
- * Synchronous concept: 
- *  for Example suppose 
-  block1
-  block2
-  block3
+const express = require("express");
+const app = express();
+app.listen("5000");
 
- * execution --> block1 run hoga aur jab iska execution complete ho jayega tab block2 and then block 3 so on.....  
- * 
+app.get('',(req,res)=>{
+    res.send("this is Home page");
+});
 
- * Asynchronous concept: 
- * second task doesn't wait for completion of first task.
- 
- * node.js is a asynchronous programming language. 
- * javascript is a asynchronous programming language.
- * 
- */  
-  
-console.log("Task 1");
+app.get('/about',(req,res)=>{
+    res.send("this is About page");
+})
 
-setTimeout(()=>{
-    console.log("Task 2");    
-},2000);
+app.get('/service', (req, res)=>{
+    res.send("this is Service page");
+})
 
-console.log("Task 3");
-
-//Output 
-    //Task 1
-    //Task 3
-    //Task 2
-
-    //-----------------------------------------------------------
-//Drowback
-
-let a=10;
-let b=0;
-setTimeout(()=>{
-    b=50;
-},2000);
-
-console.warn(a+b);
-
-//output:   10    it should be 60 but I am getting 10 as a output.
-
-//concept 2 check index1.js (How to handle aynchronous data)
