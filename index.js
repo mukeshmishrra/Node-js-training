e880dee20eb403885cdca50a0df6581854f18b37
Mukeshmishrra <mkmishra9a@gmail.com>
2022-07-05 22:18:07 +0530

    express basic fundamentals concepts

--- index.js
+++ index.js
@@ -10,10 +10,61 @@ app.get('',(req,res)=>{
 });
 
 app.get('/about',(req,res)=>{
-    res.send("this is About page");
+    // res.send("this is About page");
+    // suppose I Want to use Html here
+
+    res.send(`
+    <h1>
+        Hello i am about page with html heading tag.
+    </h1>
+    <a href="/contact"> 
+        go to contact page
+    </a>
+
+    `);
+
+
+})
+
+app.get("/contact",(req,res)=>{
+    //how to get url params
+    //suppose localhost:5000/contact?id=20
+    console.log(req.query.id); 
+
+    res.send(`
+        <h2>
+            contact us page.
+            ${ req.query.id }
+        </h2>
+        <a href="/about">go to about us page 
+        </a>
+    `);
 })
 
 app.get('/service', (req, res)=>{
-    res.send("this is Service page");
+    //res.send("this is Service page");
+    //HOW TO SEND JSON DATA 
+    res.send([
+        {
+            name: "mukesh",
+            address: "Basna",
+            mobile: "123456"
+        },
+        {
+            name: "muskan",
+            address: "delhi",
+            mobile: "12443"
+        },
+        {
+            name: "munna",
+            address: "nagpur",
+            mobile: "123996"
+        },
+        {
+            name: "bhola",
+            address: "kirandul",
+            mobile: "993456"
+        }
+    ]);
 })
 
